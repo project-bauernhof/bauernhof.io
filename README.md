1728eb7fc46249145b3ca7c83849f7f3c9e84b1a
Matthew Johnson <mattadjohnson@gmail.com>
2018-03-14 02:07:08 -0400

    Create React App skeleton with Prettier and ESLint

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bauernhof.io
 Project Bauernhof Homepage
+
+## Installation Instructions
+`cd bauernhof.io`
+`yarn install`
+`yarn start`
---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import logo from './logo.jpg'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <span className="" />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Bauernhof</h1>
+        </header>
+        <p className="App-intro" />
+      </div>
+    )
+  }
+}
+
+export default App
